e56cc1e7e1826a88d30691c418fc5bae8f8a9f0d
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-02-03 21:48:08 +0100

    Created the onButtonClick logic

--- app/src/main/java/com/slepnev/logicaapp/MainActivity.kt
+++ app/src/main/java/com/slepnev/logicaapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.slepnev.logicaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    private fun onButtonClick() {
+        var correctCount: Int = 0
+
+        val answer1 = edtInput1.text.toString()
+        val answer2 = edtInput2.text.toString()
+        val answer3 = edtInput3.text.toString()
+        val answer4 = edtInput4.text.toString()
+
+        if (answer1 == "T") correctCount++
+        if (answer2 == "F") correctCount++
+        if (answer3 == "F") correctCount++
+        if (answer4 == "F") correctCount++
+
+        Toast.makeText(this, getString(R.string.result, correctCount), Toast.LENGTH_SHORT).show()
+    }
+
 }
